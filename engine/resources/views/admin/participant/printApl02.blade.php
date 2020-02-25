1bca8e4d9b0fb6e1500b854d6c7cee390df38113
Falahlaz <alfalahlazuardi@gmail.com>
2020-02-26 06:41:08 +0700

    change printApl02

--- engine/resources/views/admin/participant/printApl02.blade.php
+++ engine/resources/views/admin/participant/printApl02.blade.php
@@ -10,7 +10,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <title>Register APL 02</title>
+    <title>Form APL 02 {{$data['participant']->registration->nama_lengkap}}</title>
   </head>
   <body>
     
